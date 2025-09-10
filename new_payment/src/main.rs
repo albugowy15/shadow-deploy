4ca9eb41ac30a33cea52dcdc91198937aec64e6a
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2025-09-10 09:53:48 +0700

    feat: setup rust project

---
+++ new_payment/src/main.rs
@@ -0,0 +1,47 @@
+use axum::{
+    Router, error_handling::HandleErrorLayer, http::StatusCode, response::Html, routing::get,
+};
+use std::time::Duration;
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/hello", get(handler)).layer(
+        ServiceBuilder::new()
+            .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                if error.is::<tower::timeout::error::Elapsed>() {
+                    Ok(StatusCode::REQUEST_TIMEOUT)
+                } else {
+                    Err((
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal server error: {error}"),
+                    ))
+                }
+            }))
+            .timeout(Duration::from_secs(10))
+            .layer(TraceLayer::new_for_http())
+            .into_inner(),
+    );
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8001")
+        .await
+        .unwrap();
+
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
